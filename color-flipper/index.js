36becedda68aef3584154a84f0e7d2d34ea80687
DJanae <djanaepatterson07@gmail.com>
2025-08-20 08:09:23 -0500

    Added rigged-slot-machine Python project

---
+++ color-flipper/index.js
@@ -0,0 +1,24 @@
+const body = document.getElementsByTagName("body")[0]
+body.style.backgroundColor = "pink"
+
+function setColor(name){
+    body.style.backgroundColor = name;
+
+}
+
+function randomColor() {
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+
+    const color = `rgb(${red},${green}, ${blue})`
+    body.style.background = color;
+
+}
+
+randomColor()
+
+
+
+
+
